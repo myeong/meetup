96d7452b891564545b3e9126884e3762e796223d
Myeong Lee <myeong@Myeongui-MacBook-Pro.local>
2015-09-08 23:33:16 -0400

    RSVP and Member added

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ if( !isset($_GET['code']) )
     //authorize and go back to URI w/ code
     $meetup = new Meetup();
     $meetup->authorize(array(
-        'client_id'     => 't97l196jncae6ljsgvejukp5b8',
-        'redirect_uri'  => 'http://meetup.myeonglee.com',     
+        // 'client_id'     => 't97l196jncae6ljsgvejukp5b8',
+        //'redirect_uri'  => 'http://meetup.myeonglee.com',  
+        "client_id"     => '27rl5urk95fgd40ehavhp4jhid',
+        "redirect_uri"  => 'http://myeonglee.com/meetup2',    
         )
     );
 }
@@ -17,9 +19,12 @@ else
     //assuming we came back here...
     $meetup = new Meetup(
         array(
-            "client_id"     => 't97l196jncae6ljsgvejukp5b8',
-            "client_secret" => 'ma98kbnuerp7fkhpcapo2eg56q',
-            "redirect_uri"  => 'http://meetup.myeonglee.com',
+            // "client_id"     => 't97l196jncae6ljsgvejukp5b8',
+            // "client_secret" => 'ma98kbnuerp7fkhpcapo2eg56q',
+            //"redirect_uri"  => 'http://meetup.myeonglee.com',
+            "client_id"     => '27rl5urk95fgd40ehavhp4jhid',
+            "client_secret" => 'l6mr9ga1u62mgk8emfn3u8s0k9',
+            "redirect_uri"  => 'http://myeonglee.com/meetup2',
             "code"          => $_GET['code'], //passed back to us from meetup
         )
     );
@@ -58,19 +63,44 @@ else
     // echo getTopicCategory($meetup);
 //     echo getTopic($meetup);
 
+/* Pulling RSVP data from Event IDs */
     $event_ids = array();
     $file = fopen("data/neigh_id.csv","r");
+    //$file = fopen("data/test.csv","r");
+    $i = 0;
 
     while(!feof($file))
     {
-        $event_ids[] = fgetcsv($file)[0];
+        $event_ids[$i] = fgetcsv($file)[0];
+        $i += 1;
     }
 
     fclose($file);    
+    $i = 0;
+    $round = 0;
+    $chunk = '';
+    print('Size: ' . strval(sizeof($event_ids)) . '</br>');
+    // print_r($event_ids);
 
 /* Get RSVP data for a give event ID */
-    echo getRSVPs($meetup, $event_ids);
+    for ($i=0; $i<sizeof($event_ids); $i++) {
+        $chunk .= strval($event_ids[$i]);
 
+        if ($i!=0 && $i % 100 == 0) {            
+            echo getRSVPs($meetup, $chunk, $round);            
+            $chunk = '';
+            $round += 1;
+            
+        } elseif ($i == sizeof($event_ids) - 1) {            
+            echo getRSVPs($meetup, $chunk, $round);            
+            $chunk = '';
+            $round += 1;
+                      
+        } else {
+            $chunk .= ',';             
+        }        
+    } 
+    
 /*
 	foreach ($response->results as $event) 
 	{
@@ -83,46 +113,112 @@ else
 */
 }
 
-function getRSVPs($meetup, $eid){
+function getRSVPs($meetup, $eids, $round){
     $output = array();    
     $total_count = 0;
-
-    foreach($eid as $event_id){
-        
-        $response = $meetup->getRSVPs(array(
-            'event_id' => $event_id,            
-        ));
-
-        foreach ($response as $item){
-            //print($output);
-            $output[$item->rsvp_id] = $event_id . ',' . $item->member->member_id . ',' . $item->member->member_city . "\r\n";
-        }
-        $total_count += $response->meta->total_count;
-
-        while ($meetup->hasNext() != null){
-            if ($response->meta->next == '') break;
-            $response = $meetup->getNext($response); 
             
-            foreach ($response as $item){
-                $output[$item->rsvp_id] = $item;
-            }
-            $total_count += $response->meta->total_count;
-            sleep(1);
-        }
-        sleep(50);
-        
-    }
-    
-    // total number of items matching the get request
-    
-    //$json_format = json_encode($output);
+    $response = $meetup->getRSVPs(array(
+        'event_id' => $eids,  
+        'rsvp' => 'yes',          
+    ));    
 
-    $fp = fopen('RSVP_' . date('Y-m-d') . '.csv', 'w');
-    fwrite($fp, $output);  
+    foreach ($response->results as $item){       
+        $str = $item->event->id . ',' . $item->member->member_id;        
+        $output[$item->rsvp_id][] = $str;            
+    }
+
+    $total_count = $response->meta->total_count;
+    print (strval($round) . ' round: ');
+
+    while ($meetup->hasNext() != null){
+        if ($response->meta->next == '') break;
+        $response = $meetup->getNext($response); 
+        // print ('  ...next response..<br>');
+        foreach ($response->results as $item){
+            $str = $item->event->id . ',' . $item->member->member_id;
+            $output[$item->rsvp_id][] = $str;   
+        }        
+        sleep(1);
+    }        
+    
+    $file_index += 1;
+    sleep(1);
+
+    getMembersReady($meetup, $output, $round);
+        
+    $fp = fopen('RSVP_' . date('Y-m-d') . '_' . strval($round) . '.csv', 'w');
+    foreach ($output as $row){        
+        fputcsv($fp, $row);  
+    }
     fclose($fp);    
     
     return $total_count . ' RSVPs pulled <br>';
 }
+
+function getMembersReady($meetup, $mids, $rsvpround){
+    $unique = array();
+
+    foreach ($mids as $key => $value) {        
+        $mid = explode(',', $value[0])[1];
+        $unique[$mid] = $mid;
+    }
+    print (strval(sizeof($unique)) . ' unique member IDs... <br>');
+
+    $chunk = '';
+    $i = 0;
+    $round = 0;
+
+    foreach ($unique as $item) {
+        $chunk .= strval($item);
+
+        if ($i!=0 && $i % 400 == 0) {            
+            echo getMembers($meetup, $chunk, $round, $rsvpround);            
+            $chunk = '';
+            $round += 1;
+            
+        } elseif ($i == sizeof($unique) - 1) {            
+            echo getMembers($meetup, $chunk, $round, $rsvpround);            
+            $chunk = '';
+            $round += 1;
+                      
+        } else {
+            $chunk .= ',';             
+        }        
+        $i += 1;
+    } 
+
+    
+}
+
+function getMembers($meetup, $mids, $round, $rsvpround){
+    $output = array();    
+    $total_count = 0;
+            
+    $response = $meetup->getMembers(array(
+        'member_id' => $mids,                   
+    ));  
+    $total_count = $response->meta->total_count;
+
+    foreach ($response->results as $item){       
+        $str = $item->id . ',' . $item->city . ',' . $item->state . ',' . $item->lat . ',' . 
+                        $item->lon;        
+        $output[$item->id][] = $str;            
+    }
+
+    $fp = fopen('Members_' . date('Y-m-d') . '_' . strval($rsvpround) . '_' . strval($round) . 
+            '.csv', 'w');
+
+    foreach ($output as $row){        
+        fputcsv($fp, $row);  
+    }
+    fclose($fp);   
+    return $total_count . ' Members pulled from ' . $rsvpround . '-' . $round. ' round<br>';
+
+}
+
+
+
+
 
 function getTopicCategory($meetup){
     $response = $meetup->getTopicCategories(array());
